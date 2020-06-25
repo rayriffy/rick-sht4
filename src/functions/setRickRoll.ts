0d89bb262d0c1321126048f653b769094b9ba7b2
Phumrapee Limpianchop <git@rayriffy.com>
2020-06-25 23:07:53 +0900

    chore: staged changes

--- src/functions/setRickRoll.ts
+++ src/functions/setRickRoll.ts
@@ -1,8 +1,8 @@
 import { getVideo } from './getVideo'
 
 export const setRickRoll = (element: HTMLElement): void => {
-  // Get selected video id
-  const videoId = getVideo()
-
   // TODO: Set element to link to that youtube video
+  // element.setAttribute("href", "https://www.youtube.com/watch?v=" + getVideo());
+  element.setAttribute("href", `https://www.youtube.com/watch?v=${getVideo()}`);
+
 }
